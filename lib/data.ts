d57adca276e4759d29ba2377c0a7203448167696
SponsorWins <aaronwinton@yahoo.com>
2025-08-20 21:08:17 -0600

    Create data.ts

---
+++ lib/data.ts
@@ -0,0 +1,11 @@
+export const listings = [
+  { league:'NBA', team:'Portland Pioneers', stadium:'Rose City Arena', asset:'Jersey Patch', price:25000 },
+  { league:'NFL', team:'Las Vegas Vipers', stadium:'Mojave Bank Stadium', asset:'LED Signage', price:32000 },
+  { league:'MLS', team:'Miami Waves', stadium:'Atlantic Energy Stadium', asset:'Pitch-Level Signage', price:18000 },
+
+  { league:'NHL', team:'Portland Glaciers', stadium:'Rose Ice Arena', asset:'Dasher-Board Signage', price:22750 },
+  { league:'MLB', team:'Portland Beavers', stadium:'Cascade Ballpark', asset:'Outfield Wall Signage', price:28900 },
+  { league:'NHL', team:'Miami Barracudas', stadium:'Ocean Ice Center', asset:'In-Ice Logo', price:19800 },
+  { league:'NBA', team:'Portland Pioneers', stadium:'Rose City Arena', asset:'Videoboard Signage', price:38500 },
+  { league:'NFL', team:'Portland Lumberjacks', stadium:'Timberline Field', asset:'Helmet Logo', price:45000 }
+];
